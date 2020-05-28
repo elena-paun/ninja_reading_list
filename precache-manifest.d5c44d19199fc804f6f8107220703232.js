c1c69a2bb1b0db7f08d2bfca14ebdb5d5d57a500
elenawq <60143171+elenawq@users.noreply.github.com>
2020-05-28 12:55:06 +0300

    Updates

---
+++ precache-manifest.d5c44d19199fc804f6f8107220703232.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a652d27c8480d4c37e0a2bd3a76a2a63",
+    "url": "/ninja_reading_list/index.html"
+  },
+  {
+    "revision": "12f038869386df95916b",
+    "url": "/ninja_reading_list/static/css/main.c78f7474.chunk.css"
+  },
+  {
+    "revision": "d5ea2f32eaa5703be429",
+    "url": "/ninja_reading_list/static/js/2.4d83187b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ninja_reading_list/static/js/2.4d83187b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f038869386df95916b",
+    "url": "/ninja_reading_list/static/js/main.af4ad916.chunk.js"
+  },
+  {
+    "revision": "7017b2ca903d784d6e2a",
+    "url": "/ninja_reading_list/static/js/runtime-main.74313ea0.js"
+  }
+]);
